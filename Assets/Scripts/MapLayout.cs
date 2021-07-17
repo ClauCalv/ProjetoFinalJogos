a50a830fd4ab82fb1b544157f23ecaad4c15341b
Claudio Quessada Cabello <claudio.cabello@lello.com.br>
2021-07-17 16:43:37 -0300

    Rascunho da cena de jogo
    aaaaa

---
+++ Assets/Scripts/MapLayout.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct MapLayout
+{
+
+    public Vector2 spawn;
+    public Vector2 finish;
+
+    public Vector2[] path;
+
+}
+
